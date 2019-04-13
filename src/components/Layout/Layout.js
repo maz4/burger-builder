1529a2aef3936c242cc4568a0490dd595b76f2f3
Marcin <zasadzkimarcin@gmail.com>
2019-04-13 07:31:58 +0100

    style nav menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 import Aux from "../../hoc/Aux";
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
-const layout = props => {
-  return (
-    <Aux>
-      <Toolbar />
-      <main className={classes.Content}>{props.children}</main>
-    </Aux>
-  );
+class Layout extends Component {
+  state = {
+    showSideDrawer: true,
+  }
+
+  sideDrawerClosedHandler() {
+    this.setState({showSideDrawer: false});
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer show
+          open={this.state.showSideDrawer} 
+          closed={this.sideDrawerClosedHandler.bind(this)}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
 }
-export default layout;
+
+export default Layout;
