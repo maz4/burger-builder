b37c1259e1b026e989f23bdb07ebd1056fbfef6b
Marcin <zasadzkimarcin@gmail.com>
2019-04-14 06:31:14 +0100

    add haburger menu and add click listner for the menu

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients).map( igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}: </span>{props.ingredients[igKey]}
-      </li>
-    )
-  })
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with fallowing ingredients:</p>
-      <ul>
-        {ingredientsSummary}
-      </ul>
-      <p>Continue to checkout?</p>
-      <p><strong>Total price: {props.price.toFixed(2)}</strong></p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
+  // This could be a functional component, doesn't have to be a class
 
-export default orderSummary;
+  render(){
+    const ingredientsSummary = Object.keys(this.props.ingredients).map( igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}: </span>{this.props.ingredients[igKey]}
+        </li>
+      )
+    });
+
+    return(
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with fallowing ingredients:</p>
+        <ul>
+          {ingredientsSummary}
+        </ul>
+        <p>Continue to checkout?</p>
+        <p><strong>Total price: {this.props.price.toFixed(2)}</strong></p>
+        <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,23 +1,29 @@
 import React, {Component} from "react";
-import Aux from "../../hoc/Aux";
-import Toolbar from '../Navigation/Toolbar/Toolbar'
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Aux from "../Aux/Aux";
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   }
 
   sideDrawerClosedHandler() {
     this.setState({showSideDrawer: false});
   }
 
+  sideDrawerTogglerHandler() {
+    this.setState( (prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer};
+    });
+  }
+
   render() {
     return (
       <Aux>
-        <Toolbar />
-        <SideDrawer show
+        <Toolbar drawerTogglerClicked={this.sideDrawerTogglerHandler.bind(this)}/>
+        <SideDrawer
           open={this.state.showSideDrawer} 
           closed={this.sideDrawerClosedHandler.bind(this)}/>
         <main className={classes.Content}>
